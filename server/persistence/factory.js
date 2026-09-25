bc5f5e8b62595c29ba04d89d40c756120065341e
cos3888 <steffenguetschow@googlemail.com>
2026-09-25 09:01:46 +0200

    KF_0.29.0 wire auth and world runtime services

--- server/persistence/factory.js
+++ server/persistence/factory.js
@@ -7,6 +7,11 @@ const { FileMetadataRepository } = require('./file-metadata-repository');
 const { FirestoreMetadataRepository } = require('./firestore-metadata-repository');
 const { WorldPersistenceService } = require('./world-persistence-service');
 const { WorldAdministrationService } = require('../services/world-administration-service');
+const { FileAuthRepository } = require('../auth/file-auth-repository');
+const { FirestoreAuthRepository } = require('../auth/firestore-auth-repository');
+const { AuthService } = require('../services/auth-service');
+const { WorldRuntimeManager } = require('../services/world-runtime-manager');
+const { WorldSessionService } = require('../services/world-session-service');
 
 function createPersistence(config) {
   const objectStore = config.objectStoreDriver === 'gcs'
@@ -17,7 +22,21 @@ function createPersistence(config) {
     : new FileMetadataRepository({ filePath: config.metadataFile });
   const worlds = new WorldPersistenceService({ objectStore });
   const administration = new WorldAdministrationService({ metadataRepository: metadata, worldPersistence: worlds });
-  return { objectStore, metadata, worlds, administration };
+  const authRepository = config.metadataDriver === 'firestore'
+    ? new FirestoreAuthRepository({ projectId: config.googleProjectId, collectionPrefix: config.collectionPrefix })
+    : new FileAuthRepository({ filePath: config.authFile });
+  const auth = new AuthService({ repository: authRepository });
+  const runtime = new WorldRuntimeManager({
+    worldPersistence: worlds,
+    metadataRepository: metadata,
+    idleMs: config.runtimeIdleMs
+  });
+  const worldSessions = new WorldSessionService({
+    metadataRepository: metadata,
+    worldPersistence: worlds,
+    runtimeManager: runtime
+  });
+  return { objectStore, metadata, worlds, administration, authRepository, auth, runtime, worldSessions };
 }
 
 module.exports = { createPersistence };
