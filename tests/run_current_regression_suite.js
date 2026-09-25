91659768523654fa1938c11fb8f21043540b31fc
cos3888 <steffenguetschow@googlemail.com>
2026-09-25 10:41:47 +0200

    KF_0.29.1 add fix regression to current suite

--- tests/run_current_regression_suite.js
+++ tests/run_current_regression_suite.js
@@ -31,7 +31,8 @@ const core=[
   'tests/run_kf_0_28_0_backend_persistence_foundation_test.js',
   'tests/run_kf_0_28_1_cloud_persistence_verification_test.js',
   'tests/run_kf_0_29_0_user_world_runtime_test.js',
-  'tests/run_kf_0_29_0_http_api_test.js'
+  'tests/run_kf_0_29_0_http_api_test.js',
+  'tests/run_kf_0_29_1_autosave_world_metadata_test.js'
 ];
 const extended=[
   'tests/run_current_ui_ownership_pipeline_test.js',
